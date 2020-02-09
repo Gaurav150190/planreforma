245f4fc7f4a9f13e0e9bc720668f337dd316671a
EX2INDIA\ggupta <ggupta@ex2india.com>
2020-02-09 13:43:29 +0530

    update code

--- js/renderImage.js
+++ js/renderImage.js
@@ -3,7 +3,7 @@ function getImage() {
     var imgData = renderer.domElement.toDataURL('image/jpeg');
     var link = document.createElement('a');
     var tt = imgData.replace('image/jpeg', 'image/octet-stream');
-    link.download = 'test.jpg';
+    link.download = 'scene.jpg';
     link.href = tt;
     link.click();
 }
